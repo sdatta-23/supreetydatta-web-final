fcaa50c749241d6414c89d9ef80e0eb9a349b31c
sdatta-23 <103424755+sdatta-23@users.noreply.github.com>
2022-08-19 14:49:44 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,208 @@
+// responsive navbar
+let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.nav-links');
+
+// responsive menu icon
+menu.addEventListener('click', function() {
+    navbar.classList.toggle('open-menu');
+    menu.classList.toggle('move');
+});
+
+// collapsing navbar when small screen
+window.onscroll = () => {
+    navbar.classList.remove('open-menu');
+    menu.classList.remove('move');
+}
+
+// When the hiring option is selected, a pay rate entry form appears.
+let hiringRadioButton = document.getElementById('hiring');
+let questionRadioButton = document.getElementById('question');
+let commentRadioButton = document.getElementById('comment');
+
+// to ensure that the form is only printed once, regardless of how many times the hiring button is hit
+var clicked = 0;
+
+// Event listeners
+hiringRadioButton.addEventListener('click', function() {
+    if (clicked == 0) {
+        generatePayRateInput();
+        clicked++;
+    }
+});
+
+questionRadioButton.addEventListener('click', function() {
+    if (clicked > 0) {
+        deletePayRateInput();
+        clicked = 0;
+    }
+});
+
+commentRadioButton.addEventListener('click', function() {
+    if (clicked > 0) {
+        deletePayRateInput();
+        clicked = 0;
+    }
+});
+
+// generating pay rate input field
+function generatePayRateInput() {
+    let break1 = document.createElement('br');
+    break1.id = 'b1';
+    let break2 = document.createElement('br');
+    break2.id = 'b2';
+    let break3 = document.createElement('br');
+    break3.id = 'b3';
+
+// label creation
+    const node1 = document.createElement("label");
+    const textNode = document.createTextNode("Expected Hourly Rate: ");
+    node1.appendChild(textNode);
+    node1.id = 'hiring-rate-label';
+
+// input field creation
+    const node2 = document.createElement("input");
+    node2.id = 'hiring-rate-input';
+    node2.type = 'number';
+    node2.step = '0.1';
+    node2.placeholder = 'Hourly Pay';
+    node2.classList.add('format')
+
+    document.querySelector(".radio-btns").appendChild(break1);
+    document.querySelector(".radio-btns").appendChild(break2);
+    document.querySelector(".radio-btns").appendChild(node1);
+    document.querySelector(".radio-btns").appendChild(break3);
+    document.querySelector(".radio-btns").appendChild(node2);
+}
+
+// deleting pay rate input field
+function deletePayRateInput() {
+    let label = document.getElementById('hiring-rate-label');
+    let input = document.getElementById('hiring-rate-input');
+    let div = document.querySelector(".radio-btns");
+    let b1 = document.getElementById('b1');
+    let b2 = document.getElementById('b2');
+    let b3 = document.getElementById('b3');
+
+    div.removeChild(b1);
+    div.removeChild(b2);
+    div.removeChild(b3);
+    div.removeChild(input);
+    div.removeChild(label);
+}
+
+// validating the form
+let messages = [];
+const form = document.getElementById('contact-form');
+const errorElement = document.getElementById('error');
+
+form.addEventListener('submit', (e) => {
+    messages = [];
+
+    // function calling
+    validateName();
+    validateEmail();
+    validateAddress();
+    validateCity();
+    validatePostalCode();
+    validateMessage();
+
+//  validating the pay rate only on clicking of hiring option 
+    if (clicked > 0) {
+        payRateValidation();
+    }
+
+ // error display 
+    if (messages.length > 0) {
+        e.preventDefault();
+        errorElement.innerHTML = `
+        <h3>Incorrect Inputs Provided:</h3>
+        <pre>${messages.join('\r\n')}</pre>
+        `;
+    }
+})
+
+form.addEventListener('reset', (e) => {
+    messages = [];
+    errorElement.innerHTML = '';
+})
+
+// name validator
+function validateName() {
+    const inputName = document.getElementById('name');
+    if(nullChecker(inputName, 'Name')) {
+        areAlphabets(inputName, '- Name should be valid - All characters should be alphabetical');
+    }
+}
+
+// email validator
+function validateEmail() {
+    const email = document.getElementById('email');
+    if (nullChecker(email, 'Email')) {
+        let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if (!(email.value.match(validRegex))) {
+            messages.push("- Email Address is Invalid");
+        }
+    }    
+}
+
+// address validator
+function validateAddress() {
+    const address = document.getElementById('address');
+    if (nullChecker(address, 'Address')) {
+        if (address.value.length < 10) {
+            messages.push("- Address should be atleast 10 characters long");
+        }
+    }
+}
+// city validator
+function validateCity() {
+    const city = document.getElementById('city');
+    if(nullChecker(city, 'City')) {
+        areAlphabets(city, '- City should be valid - All characters should be alphabetical');
+    }
+}
+//  postal validator
+function validatePostalCode() {
+    let postalCode = document.getElementById('pCode');
+    let validRegex = /^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/;
+    if (!(postalCode.value.match(validRegex))) {
+        messages.push("- Invalid Postal Code");
+    }
+}
+
+//  message validator
+function validateMessage() {
+    const message = document.getElementById('message');
+    if (nullChecker(message, 'Message')) {
+        if (address.value.length < 10) {
+            messages.push("- Message should be atleast 5 characters long");
+        }
+    }
+}
+
+// pay rate input field validator
+function payRateValidation() {
+    let payRateInput = document.getElementById('hiring-rate-input');
+    if (payRateInput.value <= 0) {
+        messages.push("- Enter an appropriate expected hourly pay rate")
+    }
+}
+
+// ensuring non- empty element
+function nullChecker(element, elementName) {
+    result = true;
+    if (element.value === '' || element.value == null) {
+        messages.push(`- ${elementName} is required`);
+        result = false;
+    }
+
+    return result;
+}
+
+// ensuring all the characters to be alphabet
+function areAlphabets(element, message) {
+    let validRegex = /^[A-Za-z\s]+$/;
+    if (!(element.value.match(validRegex))) {
+        messages.push(message);
+    }
+}
